6efee08fb9b65673fe734c7c67b8b01b77e56c84
Praveen Sharma <p.shar.18.12.2000@gmail.com>
2025-02-08 11:58:22 +0530

    Add google oauth for user to login

--- Index.js
+++ Index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const User = require("./models/User");
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,25 @@ mongoose
   })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
+
+// Save User to MongoDB
+app.post("/api/users", async (req, res) => {
+  try {
+    const { displayName, email } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = new User({ displayName, email });
+      await user.save();
+    }
+
+    res.status(200).json({ message: "User saved successfully", user });
+  } catch (error) {
+    console.error("Error saving user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
